248fcf9d100a116eb07e5c529f7f6562f1f60f6c
Michał Śmiech <michal.smiech@mlabs.pl>
2022-04-21 21:12:47 +0200

    sinogram

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from skimage import io, draw
 import math
 import matplotlib.pyplot as plt
+import numpy as np
 
-scans_count = 1000
-detectors_count = 30
+scans_count = 90
+detectors_count = 180
 detectors_span = 180
 
 detectors_span = float(detectors_span) / 360.0 * 2.0 * math.pi
@@ -17,8 +18,6 @@ img = io.imread('tomograf-zdjecia/Kropka.jpg', as_gray=True)
 center = (int(img.shape[0]/2), int(img.shape[1]/2))
 radius = center[0] - 1
 
-print(center)
-
 angle_step = 2.0 * math.pi / scans_count
 
 for step in range(scans_count):
@@ -29,20 +28,29 @@ for step in range(scans_count):
     emitter_loc_y = center[1] - emitter_loc_y
     emitter_loc = (emitter_loc_x, emitter_loc_y)
 
-    detectors_locs = []
+    detector_locs = []
+    scan_max = 0
     for i in range(detectors_count):
         detector_loc_x = int(radius * math.cos(angle + math.pi - detectors_span / 2 + i * detectors_span / (detectors_count - 1)))
         detector_loc_x += center[0]
         detector_loc_y = int(radius * math.sin(angle + math.pi - detectors_span / 2 + i * detectors_span / (detectors_count - 1)))
         detector_loc_y = center[1] - detector_loc_y
-        detectors_locs.append((detector_loc_x, detector_loc_y))
+        detector_loc = (detector_loc_x, detector_loc_y)
+        detector_locs.append(detector_loc)
 
-        rr, cc = draw.line(emitter_loc_x, emitter_loc_y, detector_loc_x, detector_loc_y)
-        img[rr, cc] = 1
+        line_nd = draw.line_nd(emitter_loc, detector_loc, endpoint=True)
+        value = 0
+        for j in range(len(line_nd[0])):
+            value += img[line_nd[0][j]][line_nd[1][j]]
+        value = value / len(line_nd[0])
+        sinogram[step].append(value)
+        scan_max = max(value, scan_max)
 
-    break
+    for i in range(detectors_count):
+        sinogram[step][i] /= scan_max
 
-io.imsave('test.jpg', img)
+
+io.imsave('test.jpg', np.array(sinogram))
 
 
 
